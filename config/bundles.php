41575435f75e29fe87088780253b6d9d7bf227a8
azizchrif <61543647+azizchrif@users.noreply.github.com>
2022-03-06 13:53:48 +0100

    Update
    Update

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Gregwar\CaptchaBundle\GregwarCaptchaBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
+    Endroid\QrCodeBundle\EndroidQrCodeBundle::class => ['all' => true],
 ];
---
+++ src/Form/CategorieEquipementType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\CategorieEquipement;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+
+class CategorieEquipementType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom_categorie_equipement', ChoiceType::class, [
+                'choices' => [
+                    'Nourriture' => 'Nourriture',
+                    'Vestimentaire' => 'Vestimentaire',
+                    'Metalique' => 'Metalique',
+                    'Plastique' => 'Plastique',
+                    'Eco-friendly' => 'Eco-friendly',
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => CategorieEquipement::class,
+        ]);
+    }
+}
